f512839562a3942f7f56532e7508eea119d614c3
NinaValkova <nina.p.valkova@gmail.com>
2025-02-05 17:16:10 +0200

    "commit programs"

---
+++ Първи стъпки в програмирането/print_hello_softuni.py
@@ -0,0 +1,6 @@
+"""
+Console program: "Hello SoftUni"
+This program prints "Hello SoftUni" to the console.
+"""
+
+print("Hello SoftUni")
---
+++ Първи стъпки в програмирането/rectangle_area.py
@@ -0,0 +1,9 @@
+"""
+Calculating the area of ​​a rectangle
+Write a console program that inputs two integers (the sides of the rectangle a and b) and calculates the area of ​​a rectangle with these sides.
+"""
+
+a = int(input())
+b = int(input())
+area = a * b
+print(area)
